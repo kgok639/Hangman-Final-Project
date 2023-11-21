3c8a28229423aa50ccaf50835269d65f5da3c679
Kerim GÖK <kgok639@gmail.com>
2023-11-21 19:23:27 +0300

    Initial commit

---
+++ hangman_words.py
@@ -0,0 +1,11 @@
+word_list = [
+    "masa", "sandalye", "ağaç", "tahta", "kurabiye", "ayran", "şişe", "kitap",
+    "kalem", "kukla", "çaydanlık", "kalemlik", "silgi", "mürekkep", "patates",
+    "soğan", "ekmek", "marul", "tavuk", "inek", "kedi", "köpek", "kurt",
+    "aslan", "kaplan", "maymun", "orangutan", "bonobo", "tarih", "spor",
+    "tablet", "telefon", "bilgisayar", "televizyon", "kumanda", "klavye",
+    "sehpa", "kanepe", "koltuk", "halı", "battaniye", "örtü", "kapı",
+    "bilgisayar", "okul", "üniversite", "kaldırım", "otomobil", "market",
+    "mağaza", "bakkal", "kase", "beyin", "kalp", "karaciğer", "akciğer",
+    "böbrek", "kablo"
+]
